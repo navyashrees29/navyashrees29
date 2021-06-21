32f51291884b1319ca748b60356a99105fdcf34a
navyashrees29 <86050652+navyashrees29@users.noreply.github.com>
2021-06-21 20:19:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo student1 = new StudentInfo();
+            student1.setId("101");
+            student1.setName("Abhi");
+            student1.setMarks(25);
+
+            System.Console.WriteLine("Student1 id is "+ student1.getId());
+            System.Console.WriteLine("Student1 name is "+ student1.getName());
+            System.Console.WriteLine("Student1 marks is "+ student1.getMarks());
+
+            StudentInfo student2 = new StudentInfo();
+            student2.setId("102");
+            student2.setName("Arun");
+            student2.setMarks(50);
+
+            System.Console.WriteLine("Student2 id is "+ student2.getId());
+            System.Console.WriteLine("Student2 name is "+ student2.getName());
+            System.Console.WriteLine("Student2 marks is "+ student2.getMarks());
+
+        }
+    }
+}
---
+++ StudentInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class StudentInfo
+    {
+        private string id;
+        private string name;
+        private int marks;
+
+        public string getId()
+        {
+            return this.id;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public int getMarks()
+        {
+            return this.marks;
+        }
+
+        public void setId(string id)
+        {
+            this.id = id;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setMarks(int marks)
+        {
+            this.marks = marks;
+        }
+
+    }
+}
