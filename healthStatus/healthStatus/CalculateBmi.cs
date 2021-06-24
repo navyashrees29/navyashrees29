32acaca9e96843e1676e60ae0d9e0cf2e1478f08
navyashrees29 <navya9529@gmail.com>
2021-06-24 17:19:41 +0530

    commit

---
+++ healthStatus/healthStatus/CalculateBmi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthStatus
+{
+    class CalculateBmi
+    {
+      /*  private float height;
+        private float weight;
+        private float bmiValue;
+
+        public CalculateBmi(float height, float weight)
+        {
+            this.height = height;
+            this.weight = weight;
+
+        }
+
+        public float getbmiValue()
+        {
+            return this.bmiValue;
+        }
+
+        public void setbmiValue(float bmiValue)
+        {
+            this.bmiValue = bmiValue;
+        }
+
+      
+    */
+    }
+}
